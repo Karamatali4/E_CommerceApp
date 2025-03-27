a2357d3910bcb9a22c6d5838c224bf383547213d
Karamatali4 <maqpoon4@gmail.com>
2025-03-27 16:25:24 +0500

    add database

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect();
+        await mongoose.connect(`${process.env.ECOMMERCEDB}`);
         console.log('Database connected successfully');
     } catch (error) {
         console.log(error);
