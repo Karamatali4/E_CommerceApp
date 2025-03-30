0f5eb95cd75662e47b5395b44a49beb335462e15
Karamatali4 <maqpoon4@gmail.com>
2025-03-30 15:08:48 +0500

    changes in db file

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(`${process.env.ECOMMERCEDB}`);
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(`${process.env.ECOMMERCEDB}`);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = dbConnection;
