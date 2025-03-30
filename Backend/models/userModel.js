249043ec8d3ae761fcfd1163d0e0820edf6d0785
Karamatali4 <maqpoon4@gmail.com>
2025-03-30 15:09:27 +0500

    creating model folder and file uer and cart

---
+++ Backend/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    gender: { type: String, enum: ["Male", "Female", "Other"], required: true }, 
+    phone: { type: String, required: true, match: /^[0-9]{10,15}$/ },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+// create user Collection
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
