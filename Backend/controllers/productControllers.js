7b1d7620a0644b59f0f5724ad6e0d4d0f4e400fe
Karamatali4 <maqpoon4@gmail.com>
2025-04-14 23:59:01 +0500

    creating product controller file and create product function for create product

---
+++ Backend/controllers/productControllers.js
@@ -0,0 +1,16 @@
+
+import Product from "../models/Product.js";
+
+
+
+const productAdd =  async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    const saved = await newProduct.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = productAdd;
