4c1d9d2b0e0f10d677efc549184e6c7cd0ec9810
Karamatali4 <maqpoon4@gmail.com>
2025-04-19 23:46:28 +0500

    add routes  in cart route for delete all product and remove single product  in cart

--- Backend/routes/cartRoutes.js
+++ Backend/routes/cartRoutes.js
@@ -14,10 +14,17 @@
 
 const express = require("express");
 const { verifyToken } = require("../middleware/authMiddleware.js");
-const addToCart = require("../controllers/cartControllers.js"); // ✅ No destructuring
+const {addToCart, deleteCart, removeFromCart} = require("../controllers/cartControllers.js"); 
 
 const cartRoutes = express.Router();
 
+// for add cart route
 cartRoutes.post("/add", addToCart);
+
+// for delete cart route
+cartRoutes.delete("/delete", verifyToken, deleteCart);
+
+// for delete one product
+cartRoutes.delete("/remove/:productId", verifyToken, removeFromCart);
 
 module.exports = cartRoutes;
