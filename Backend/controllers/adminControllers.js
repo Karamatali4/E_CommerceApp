eca4b399770df47971a11e28b9f054c90dc626d2
Karamatali4 <maqpoon4@gmail.com>
2025-04-21 17:01:58 +0500

    add functions such as deleteSingleUser, updateSingleUser deleteSingleProduct

--- Backend/controllers/adminControllers.js
+++ Backend/controllers/adminControllers.js
@@ -25,6 +25,54 @@ const getAllUserData = async (req, res) => {
   }
 };
 
+
+
+// Delete user (by ID) 
+const deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser)
+      return res.status(404).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully" });
+    console.log(deletedUser.name," has Deleted in database");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+// Update user (by ID) 
+const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    ).select("-password");
+    if (!updatedUser)
+      return res.status(404).json({ message: "User not found" });
+    res.status(200).json(updatedUser);
+    console.log(updatedUser.name," has Updated in database");
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+// Delete single product 
+const deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct)
+      return res.status(404).json({ message: "Product not found" });
+    res.status(200).json({ message: "Product deleted successfully" });
+    console.log(deletedProduct.name," has deleted in database");
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 // Get All products
 const getAllProduct = async (req, res) => {
   try {
@@ -36,4 +84,4 @@ const getAllProduct = async (req, res) => {
   }
 };
 
-module.exports = { getSingleUser, getAllUserData, getAllProduct };
+module.exports = { getSingleUser, getAllUserData, deleteUser, updateUser, getAllProduct,deleteProduct };
