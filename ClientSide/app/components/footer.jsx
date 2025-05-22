991a8a440e28334e2ae91f303251d4eb316aac30
Karamatali4 <maqpoon4@gmail.com>
2025-05-22 22:24:56 +0500

    changes in footer component

--- ClientSide/app/components/footer.jsx
+++ ClientSide/app/components/footer.jsx
@@ -4,45 +4,48 @@ import "../components/style.css";
 export default function Footer() {
   return (
     <>
-      <footer className="footer w-[100%] bg-black h-52 flex justify-evenly items-center">
-        <box className="box1">
-            <ul>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-            </ul>
-            
-        </box>
-        <box className="box2">
-            <ul>
-                <li className="text-lime-50">Exclusive</li>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-            </ul>
-        </box>
-        <box className="box3">
-            <ul>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-            </ul>
-        </box>
-        <box className="box4">
-            <ul>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-            </ul>
-        </box>
-        <box className="box5">
-            <ul>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-                <li><Link to="#"> </Link> </li>
-            </ul>
-        </box>
+      <footer className="bg-black text-white p-6 grid grid-cols-2 md:grid-cols-4 gap-16 text-sm">
+        <div>
+          <h3 className="font-bold mb-2">Exclusive</h3>
+          <p>Get 10% off your first order</p>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="mt-2 px-3 py-1 rounded text-black"
+          />
+        </div>
+
+        <div>
+          <h3 className="font-bold mb-2">Support</h3>
+          <p>111 Bijoy sarani, Dhaka, BD</p>
+          <p>exclusive@gmail.com</p>
+          <p>+88015-88888-9999</p>
+        </div>
+
+        <div>
+          <h3 className="font-bold mb-2">Account</h3>
+          <p>My Account</p>
+          <p>Login / Register</p>
+          <p>Cart</p>
+          <p>Wishlist</p>
+          <p>Shop</p>
+        </div>
+
+        <div>
+          <h3 className="font-bold mb-2">Quick Link</h3>
+          <p>Privacy Policy</p>
+          <p>Terms Of Use</p>
+          <p>FAQ</p>
+          <p>Contact</p>
+        </div>
+
+        
       </footer>
+
+      {/* Copyright */}
+      <div className="bg-black text-white text-center py-3 text-xs">
+        © Copyright Rimel 2022. All rights reserved.
+      </div>
     </>
   );
 }
